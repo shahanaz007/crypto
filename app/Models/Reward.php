3fa96326f41f86889a339474ab6dc288cdb02b46
shahn <shahanaz345@gmail.com>
2021-10-18 10:51:20 +0530

    .

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
+use App\Models\{User,tokens_usdt_wallet};
+use Carbon\Carbon;
+
 class Reward extends Model
 {
     use HasFactory;
@@ -26,9 +28,12 @@ class Reward extends Model
         return $this->belongsTo('App\Models\User','user_id');
     }
 
-    public function give_reward($user_id,$amount,$coin,$remarks=null)
+    // give reward for a single user
+    public function give_reward_for($user_id,$amount,$coin,$remarks=null)
     {   
         // store reward log
+        $date   = Carbon::now()->format('Y-m-d');
+
         $rewards          = new Reward;
         $rewards->user_id = $user_id;
         $rewards->coin    = $coin;
@@ -38,10 +43,10 @@ class Reward extends Model
         $rewards->save();
 
         // store reward on wallet
-        
+        tokens_usdt_wallet::credit($user_id,$amount,$coin,$remarks);
     }
 
-    public static function give_rewards_for_referees($user_id,$amount,$coin)
+    public static function give_rewards_for_referees_of($user_id,$amount)
     {
         $user = User::find($user_id);
         $purchased_amount = $amount;
@@ -53,15 +58,19 @@ class Reward extends Model
 
             // giving rewards based on refer levels 
             for($i=1; $i<=$rewarding_levels; $i++) {
+                if($tmp_user_id)
+                {
+                    $tmp_user = User::find($tmp_user_id);
+                    $reward   = config('app.reward_for_level'.$i);
+                    $coin     = config('app.reward_coin_for_token_purchase');
+                    $remarks  = "Reward for sales";
 
-                $tmp_user = User::find($tmp_user_id);
-                $reward   = config('app.reward_for_level'.$i);
-                $coin     = config('app.reward_coin_for_token_purchase');
-                $remarks  = "Reward for sales";
+                    // give reward for this referee
+                    $this->give_reward_for($tmp_user->id,$reward,$coin,$remarks);
 
-                $this->give_reward($tmp_user->id,$reward,$coin,$remarks)
-
-                $tmp_user_id = $tmp_user->referby;
+                    //for taking next level referee
+                    $tmp_user_id = $tmp_user->referby;
+                }
             }
         }
 
@@ -69,7 +78,7 @@ class Reward extends Model
 
     }
 
-
+    // find reward leves based on purchase/ sales amount
     public function get_levels($amount)
     {
         $silver_start = config('app.silver_start'); 
