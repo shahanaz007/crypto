6f213831a034ba0f2c6e13e8066adb86b4b7d97d
Thushara Thulasi <thushara@techanise.com>
2021-10-19 23:24:37 +0530

    ..

---
+++ resources/views/include/admin/sidebar.blade.php
@@ -0,0 +1,399 @@
+<aside class="main-sidebar">
+    <!-- sidebar -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+		 <div class="ulogo">
+			 <a href="index.html">
+			  <!-- logo for regular state and mobile devices -->
+			  <span><b>Crypto </b>Admin</span>
+			</a>
+		</div>
+        <div class="image">
+          <img src="{{asset('assets/back-end/images/user2-160x160.jpg')}}" class="rounded-circle" alt="User Image">
+        </div>
+        <div class="info">
+          <p>Admin Template</p>
+			<a href="" class="link" data-toggle="tooltip" title="" data-original-title="Settings"><i class="ion ion-gear-b"></i></a>
+            <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Email"><i class="ion ion-android-mail"></i></a>
+            <a href="" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ion ion-power"></i></a>
+        </div>
+      </div>
+      <!-- sidebar menu -->
+      <ul class="sidebar-menu" data-widget="tree">
+		<li class="nav-devider"></li>
+        <li class="header nav-small-cap">PERSONAL</li>
+        <li class="active">
+          <a href="index.html">
+            <i class="icon-home"></i> <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-chart"></i>
+            <span>Reports</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/reports/transactions.html">Transactions</a></li>
+            <li><a href="pages/reports/top-gainers-losers.html">Top Gainers/Losers</a></li>
+            <li><a href="pages/reports/market-capitalizations.html">Market Capitalizations</a></li>
+            <li><a href="pages/reports/crypto-stats.html">Crypto Stats</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-compass"></i>
+            <span>Initial Coin Offering</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/ico/ico-distribution-countdown.html">Countdown</a></li>
+            <li><a href="pages/ico/ico-roadmap-timeline.html">Roadmap/Timeline</a></li>
+            <li><a href="pages/ico/ico-progress.html">Progress Bar</a></li>
+            <li><a href="pages/ico/ico-details.html">Details</a></li>
+            <li><a href="pages/ico/ico-listing.html">ICO Listing</a></li>
+            <li><a href="pages/ico/ico-filter.html">ICO Listing - Filters</a></li>			  
+          </ul>
+        </li>
+        <li>
+          <a href="pages/currency-ex/exchange.html">
+            <i class="icon-refresh"></i> <span>Currency Exchange</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-people"></i>
+            <span>Members</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/members/members.html">Members Grid</a></li>
+            <li><a href="pages/members/members-list.html">Members List</a></li>
+            <li><a href="pages/members/member-profile.html">Member Profile</a></li>			  
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-equalizer"></i>
+            <span>Tickers</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/tickers/tickers.html">Ticker</a></li>
+            <li><a href="pages/tickers/crypto-live-pricing.html">Live Crypto Prices</a></li>		  
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-wallet"></i>
+            <span>Transactions</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/transactions/tables-transactions.html">Transactions Tables</a></li>
+            <li><a href="pages/transactions/transaction-search.html">Transactions Search</a></li>	
+            <li><a href="pages/transactions/transaction-details.html">Single Transaction</a></li>
+            <li><a href="pages/transactions/counter-transactions.html">Transactions Counter</a></li>	  
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-graph"></i>
+            <span>Charts</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/charts/chartjs.html">ChartJS</a></li>
+            <li><a href="pages/charts/flot.html">Flot</a></li>
+            <li><a href="pages/charts/inline.html">Inline charts</a></li>
+            <li><a href="pages/charts/morris.html">Morris</a></li>
+            <li><a href="pages/charts/peity.html">Peity</a></li>
+            <li><a href="pages/charts/chartist.html">Chartist</a></li>
+            <li><a href="pages/charts/rickshaw-charts.html">Rickshaw Charts</a></li>
+            <li><a href="pages/charts/nvd3-charts.html">NVD3 Charts</a></li>
+			<li><a href="pages/charts/echart.html">eChart</a></li>
+			<li><a href="pages/amcharts/amcharts.html">amCharts Charts</a></li>
+			<li><a href="pages/amcharts/amstock-charts.html">amCharts Stock Charts</a></li>
+			<li><a href="pages/amcharts/ammaps.html">amCharts Maps</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-grid"></i>
+            <span>App</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/app/app-chat.html">Chat app</a></li>
+            <li><a href="pages/app/project-table.html">Project</a></li>
+            <li><a href="pages/app/app-contact.html">Contact / Employee</a></li>
+            <li><a href="pages/app/app-ticket.html">Support Ticket</a></li>
+			<li><a href="pages/app/calendar.html">Calendar</a></li>
+            <li><a href="pages/app/profile.html">Profile</a></li>
+            <li><a href="pages/app/userlist-grid.html">Userlist Grid</a></li>
+			<li><a href="pages/app/userlist.html">Userlist</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-envelope"></i> <span>Mailbox</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/mailbox/mailbox.html">Inbox</a></li>
+            <li><a href="pages/mailbox/compose.html">Compose</a></li>
+            <li><a href="pages/mailbox/read-mail.html">Read</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-drop"></i>
+            <span>UI Elements</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/UI/badges.html">Badges</a></li>
+            <li><a href="pages/UI/border-utilities.html">Border</a></li>
+            <li><a href="pages/UI/buttons.html">Buttons</a></li>
+            <li><a href="pages/UI/bootstrap-switch.html">Bootstrap Switch</a></li>
+            <li><a href="pages/UI/cards.html">User Card</a></li>
+            <li><a href="pages/UI/color-utilities.html">Color</a></li>
+			<li><a href="pages/UI/date-paginator.html">Date Paginator</a></li>
+            <li><a href="pages/UI/dropdown.html">Dropdown</a></li>
+            <li><a href="pages/UI/dropdown-grid.html">Dropdown Grid</a></li>
+            <li><a href="pages/UI/general.html">General</a></li>
+            <li><a href="pages/UI/icons.html">Icons</a></li>
+            <li><a href="pages/UI/media-advanced.html">Advanced Medias</a></li>
+			<li><a href="pages/UI/modals.html">Modals</a></li>
+            <li><a href="pages/UI/nestable.html">Nestable</a></li>
+            <li><a href="pages/UI/notification.html">Notification</a></li>
+            <li><a href="pages/UI/portlet-draggable.html">Draggable Portlets</a></li>
+            <li><a href="pages/UI/ribbons.html">Ribbons</a></li>
+            <li><a href="pages/UI/sliders.html">Sliders</a></li>
+            <li><a href="pages/UI/sweatalert.html">Sweet Alert</a></li>
+            <li><a href="pages/UI/tab.html">Tabs</a></li>
+            <li><a href="pages/UI/timeline.html">Timeline</a></li>
+            <li><a href="pages/UI/timeline-horizontal.html">Horizontal Timeline</a></li>			  
+          </ul>
+        </li>
+        <li class="header nav-small-cap">FORMS, TABLE & LAYOUTS</li>
+		<li class="treeview">
+          <a href="#">
+            <i class="icon-puzzle"></i>
+            <span>Widgets</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/widgets/blog.html">Blog</a></li>
+            <li><a href="pages/widgets/chart.html">Chart</a></li>
+            <li><a href="pages/widgets/list.html">List</a></li>
+            <li><a href="pages/widgets/social.html">Social</a></li>
+            <li><a href="pages/widgets/statistic.html">Statistic</a></li>
+            <li><a href="pages/widgets/tiles.html">Tiles</a></li>
+            <li><a href="pages/widgets/weather.html">Weather</a></li>
+            <li><a href="pages/widgets/widgets.html">Widgets</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-layers"></i>
+            <span>Layout Options</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/layout/boxed.html">Boxed</a></li>
+            <li><a href="pages/layout/fixed.html">Fixed</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html">Collapsed Sidebar</a></li>
+          </ul>
+        </li>		
+		<li class="treeview">
+          <a href="#">
+            <i class="icon-loop"></i>
+            <span>Box</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/box/advanced.html">Advanced</a></li>
+            <li><a href="pages/box/basic.html">Boxed</a></li>
+            <li><a href="pages/box/color.html">Color</a></li>
+			<li><a href="pages/box/group.html">Group</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-note"></i> <span>Forms</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/forms/advanced.html">Advanced Elements</a></li>
+            <li><a href="pages/forms/code-editor.html">Code Editor</a></li>
+            <li><a href="pages/forms/editor-markdown.html">Markdown</a></li>
+            <li><a href="pages/forms/editors.html">Editors</a></li>
+            <li><a href="pages/forms/form-validation.html">Form Validation</a></li>
+            <li><a href="pages/forms/form-wizard.html">Form Wizard</a></li>
+            <li><a href="pages/forms/general.html">General Elements</a></li>
+            <li><a href="pages/forms/mask.html">Formatter</a></li>
+            <li><a href="pages/forms/premade.html">Pre-made Forms</a></li>
+            <li><a href="pages/forms/xeditable.html">Xeditable Editor</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-menu"></i> <span>Tables</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/tables/simple.html">Simple tables</a></li>
+            <li><a href="pages/tables/data.html">Data tables</a></li>
+            <li><a href="pages/tables/editable-tables.html">Editable Tables</a></li>
+            <li><a href="pages/tables/table-color.html">Table Color</a></li>
+          </ul>
+        </li>
+		<li>
+          <a href="pages/email/index.html">
+            <i class="icon-envelope-open"></i> <span>Emails</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+        </li>
+		<li class="header nav-small-cap">EXTRA COMPONENTS</li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-map"></i> <span>Map</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/map/map-google.html">Google Map</a></li>
+            <li><a href="pages/map/map-vector.html">Vector Map</a></li>
+          </ul>
+        </li>
+		<li class="treeview">
+          <a href="#">
+            <i class="icon-magnet"></i> <span>Extension</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/extension/fullscreen.html">Fullscreen</a></li>
+          </ul>
+        </li>        
+		<li class="treeview">
+          <a href="#">
+            <i class="icon-folder-alt"></i> <span>Sample Pages</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/samplepage/blank.html">Blank</a></li>
+            <li><a href="pages/samplepage/coming-soon.html">Coming Soon</a></li>
+            <li><a href="pages/samplepage/custom-scroll.html">Custom Scrolls</a></li>
+			<li><a href="pages/samplepage/faq.html">FAQ</a></li>
+			<li><a href="pages/samplepage/gallery.html">Gallery</a></li>
+			<li><a href="pages/samplepage/invoice.html">Invoice</a></li>
+			<li><a href="pages/samplepage/lightbox.html">Lightbox Popup</a></li>
+			<li><a href="pages/samplepage/pace.html">Pace</a></li>
+			<li><a href="pages/samplepage/pricing.html">Pricing</a></li>
+            <li class="treeview">
+              <a href="#">Authentication 
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="pages/samplepage/login.html">Login</a></li>
+                <li><a href="pages/samplepage/register.html">Register</a></li>
+                <li><a href="pages/samplepage/lockscreen.html">Lockscreen</a></li>
+                <li><a href="pages/samplepage/user-pass.html">Recover password</a></li>				  
+              </ul>
+            </li>            
+			<li class="treeview">
+              <a href="#">Error Pages 
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="pages/samplepage/404.html">404</a></li>
+                <li><a href="pages/samplepage/500.html">500</a></li>
+                <li><a href="pages/samplepage/maintenance.html">Maintenance</a></li>		  
+              </ul>
+            </li> 
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="icon-menu"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Level One</a></li>
+            <li class="treeview">
+              <a href="#">Level One
+                <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="#">Level Two</a></li>
+                <li class="treeview">
+                  <a href="#">Level Two
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-right pull-right"></i>
+            </span>
+                  </a>
+                  <ul class="treeview-menu">
+                    <li><a href="#">Level Three</a></li>
+                    <li><a href="#">Level Three</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li><a href="#">Level One</a></li>
+          </ul>
+        </li>        
+        
+      </ul>
+    </section>
+  </aside>
--- routes/web.php
+++ routes/web.php
@@ -168,5 +168,5 @@ Route::resource('stake_tokens','StakeTokensController');
 Route::get('stake_tokens/unstake/{id}','StakeTokensController@unstake');
 
 Route::view('dashboard','dashboard');
-Route::view('defaut','layouts.admin_default');
+Route::view('admin_dashboard','admin.dashboard');
  Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
