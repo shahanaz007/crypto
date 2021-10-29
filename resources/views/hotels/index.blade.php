2817861b6031d8c841a94c99408710faa8d1fbc9
Thushara128 <thushara.t240@gmail.com>
2021-10-29 15:03:18 +0530

    '..'

--- resources/views/hotels/index.blade.php
+++ resources/views/hotels/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.default')
+@extends('layouts.user_layout')
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 <style>
 	.search-sec{
@@ -97,7 +97,36 @@ input[type="number"] {
 }
 
 </style>
-@section('content')
+@section('content')<section class="bg-img pt-150" data-overlay="7" style="background-image: url(../images/front-end-img/background/bg-8.jpg);">
+    <!-- <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="text-center">           
+            <h2 class="page-title text-white">Buy And Sell</h2>
+            <ol class="breadcrumb bg-transparent justify-content-center">
+              <li class="breadcrumb-item"><a href="#" class="text-white-50"><i class="mdi mdi-home-outline"></i></a></li>
+              <li class="breadcrumb-item text-white active" aria-current="page">Buy and Sell</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </section>
+<section class="bg-img pt-150" data-overlay="7" style="background-image: url(../images/front-end-img/background/bg-8.jpg);">
+    <!-- <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="text-center">           
+            <h2 class="page-title text-white">Buy And Sell</h2>
+            <ol class="breadcrumb bg-transparent justify-content-center">
+              <li class="breadcrumb-item"><a href="#" class="text-white-50"><i class="mdi mdi-home-outline"></i></a></li>
+              <li class="breadcrumb-item text-white active" aria-current="page">Buy and Sell</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </section>
 <section class="pt-130 pb-50">
 <div class="container">
     <div class="row justify-content-center">
