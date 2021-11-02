fd8318ed1711bba69f15a605cd416779f1de21a5
chippysurendran <chippy@techanise.com>
2021-11-02 20:29:59 +0530

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Cookie;
+use App\Models\{Coupon};
 
 class Controller extends BaseController
 {
@@ -13,6 +15,10 @@ class Controller extends BaseController
 
     public function __construct()
     {
-
+    	
+    	if (Cookie::get('region_id') == null){
+    		$region = Coupon::select('location_id')->where('used','=',0)->first();
+    		Cookie::queue('region_id', $region);
+    	}
     }
 }
