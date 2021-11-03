729e628c6d4af33660cf1cf36cd820b357133a1f
shahn <shahanaz345@gmail.com>
2021-11-03 20:24:18 +0530

    .

--- resources/views/user_home.blade.php
+++ resources/views/user_home.blade.php
@@ -8,6 +8,64 @@
         margin-right: 18px;
         padding-bottom: 0px;
     }
+
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  position: fixed;
+  /*bottom: 0;*/
+  /*right: 50%;*/
+  top: 10%;
+  left: 45%;
+  float: none;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+  box-shadow: 0px 0px 4px -1px;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+.form-container input[type=text], .form-container input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* When the inputs get focus, do something */
+.form-container input[type=text]:focus, .form-container input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #04AA6D;
+  color: white;
+  /*padding: 16px 20px;*/
+  border: none;
+  cursor: pointer;
+  /*width: 100%;*/
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
 </style>
 <section class="pt-130 pb-50" style="min-height:850px">
 <div style="text-align: center;color:white;font-size:40px; padding-bottom: 20px;">
@@ -81,7 +139,15 @@
                 <!-- TradingView Widget END -->
             </div>          
         </div>
-   
+   <div class="form-popup" id="myForm">
+    <div class="form-container">
+   <center> <form>
+    <h4>Referral Link Copied.</h4>
+        <button type="button" class="btn btn-warning" onclick="closeForm()">Close</button>
+</form></center>
+</div></div>
+
+
 </div>
 
 
@@ -106,6 +172,16 @@
         var copyText = $('#ref_link').html();
 
          copyToClipboard(copyText);
+         openForm();
     }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
 </script>
 @endsection
