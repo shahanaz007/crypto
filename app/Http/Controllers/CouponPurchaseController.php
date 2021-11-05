489ba590f5ecbe3eb1a7f07090aced0415c107c9
shahn <shahanaz345@gmail.com>
2021-11-05 14:15:02 +0530

    .

--- app/Http/Controllers/CouponPurchaseController.php
+++ app/Http/Controllers/CouponPurchaseController.php
@@ -11,6 +11,13 @@ use Cookie;
 
 class CouponPurchaseController extends Controller
 {
+
+    public function __construct()
+    {
+
+        $this->middleware(['auth','verified']);
+    }
+    
     /**
      * Display a listing of the resource.
      *
