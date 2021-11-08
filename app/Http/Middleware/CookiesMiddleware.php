aa545a06e4dbe044ac8d580f44fb1d71140dad47
shahn <shahanaz345@gmail.com>
2021-11-08 19:10:59 +0530

    .

--- app/Http/Middleware/CookiesMiddleware.php
+++ app/Http/Middleware/CookiesMiddleware.php
@@ -23,12 +23,20 @@ class CookiesMiddleware
                 $region = Location::first();
                 Cookie::queue('region_id', $region->id);
                 session(['region_id' => $region->id]);
+              }else if(session('region_id')  == null )
+              {
+                $region = Cookie::get('region_id');
+                session(['region_id' => $region]);
               }
 
               if (Cookie::get('currency') == null){
                 Cookie::queue('currency', 'USD');
                 session(['currency' => 'USD']);
                 // return "ok";
+              }else if(session('currency')  == null )
+              {
+                $currency = Cookie::get('currency');
+                session(['currency' => $currency]);
               }
 
 // $value = session('key');
