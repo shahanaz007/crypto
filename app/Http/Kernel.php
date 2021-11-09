d94b005b4e89e204b84093991ad641d9db8a3dbe
chippysurendran <chippy@techanise.com>
2021-11-09 11:23:51 +0530

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'Currency' => \App\Http\Middleware\CookiesMiddleware::class,
+        'admin' => \App\Http\Middleware\Admin::class,
     ];
 }
---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $admin = 'admin?';
+        if(Auth::user()->$admin == 0 ){
+            
+            abort(403);
+        }
+       
+        return $next($request);
+    }
+}
